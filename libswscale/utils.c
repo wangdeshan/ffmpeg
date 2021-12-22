70e6afd1d98d9bbccbe09d941f0047f364c6e857
wangdeshan <wangdeshan@github>
2021-12-23 01:54:59 +0800

    MOD

--- libswscale/utils.c
+++ libswscale/utils.c
@@ -1297,10 +1297,10 @@ av_cold int sws_init_context(SwsContext *c, SwsFilter *srcFilter,
 
     c->srcRange |= handle_jpeg(&c->srcFormat);
     c->dstRange |= handle_jpeg(&c->dstFormat);
-
+/*
     if(srcFormat!=c->srcFormat || dstFormat!=c->dstFormat)
         av_log(c, AV_LOG_WARNING, "deprecated pixel format used, make sure you did set range correctly\n");
-
+*/
     if (!c->contrast && !c->saturation && !c->dstFormatBpp)
         sws_setColorspaceDetails(c, ff_yuv2rgb_coeffs[SWS_CS_DEFAULT], c->srcRange,
                                  ff_yuv2rgb_coeffs[SWS_CS_DEFAULT],
